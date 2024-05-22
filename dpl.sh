cb67b9b81bf60e8b89810fb22352d6c1fe4c4e50
savelevmipt <100207961+savelevmipt@users.noreply.github.com>
2024-05-23 00:02:46 +0300

    Update dpl.sh

--- dpl.sh
+++ dpl.sh
@@ -1,6 +1,6 @@
 docker build -t main -f dockerfile_main .
 docker build -t stat -f dockerfile_stat .
 
-kubectl apply -f /Users/muradgamzatov/Desktop/ppprp/deployment_main.yaml
-kubectl apply -f /Users/muradgamzatov/Desktop/ppprp/service.yaml
-kubectl apply -f /Users/muradgamzatov/Desktop/ppprp/deployment_stat.yaml
+kubectl apply -f deployment_main.yaml
+kubectl apply -f service.yaml
+kubectl apply -f deployment_stat.yaml
